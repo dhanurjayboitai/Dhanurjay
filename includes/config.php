0ca8f5376293b41ecbdd6be775c218a18be82933
dhanurjayboitai <124765521+dhanurjayboitai@users.noreply.github.com>
2024-07-29 11:22:29 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,9 +5,9 @@ define('DB_PASS' ,'');
 define('DB_NAME', 'db_form');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
-if (mysqli_connect_errno())
+if ($con->mysqli_connect_error())
 {
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ echo "Failed to connect to MySQL: " . $con->mysqli_connect_error();
 }
 else{
     echo "Connected";
